c75ef25b2ef44f26d28d4226d5b22fa5b55c2e7c
Daniel Silva <daniel.silva@inf.ufrgs.br>
2014-11-29 11:33:14 -0200

    code generation: added ILOC instruction code generation

--- include/comp_iloc.h
+++ include/comp_iloc.h
@@ -1,5 +1,60 @@
+enum _op_code_t
+{
+	ADD,
+	SUB,
+	MULT,
+	DIV,
+	ADDI,
+	SUBI,
+	RSUBI,
+	MULTI,
+	DIVI,
+	RDIVI,
+	LSHIFT,
+	LSHIFTI,
+	RSHIFT,
+	RSHIFTI,
+	LOAD,
+	LOADAI,
+	LOADAO,
+	CLOAD,
+	CLOADAI,
+	CLOADAO,
+	LOADI,
+	STORE,
+	STOREAI,
+	STOREAO,
+	CSTORE,
+	CSTOREAI,
+	CSTOREAO,
+	I2I,
+	C2C,
+	C2I,
+	I2C,
+	CMPLT,
+	CMPLE,
+	CMPEQ,
+	CMPGE,
+	CMPGT,
+	CMPNE,
+	CBR,
+	JUMPI,
+	JUMP,
+	AND,
+	ANDI,
+	OR,
+	XOR,
+	ORI,
+	XORI,
+	NOP
+};
+
+typedef enum _op_code_t op_code_t;
+
 char *new_register(void);
 char *new_label(void);
+void *generate_instruction_code(op_code_t op_code, char *target, int n_args, ...);
+char *operation2string(op_code_t op_code);
 
 extern int register_counter;
 extern int label_counter;
--- src/comp_iloc.c
+++ src/comp_iloc.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 #include "main.h"
 
 int register_counter = 0;
@@ -21,4 +22,147 @@ char *new_label(void)
 	sprintf(new_label, "L%d:", label_counter);
 
 	return strdup(new_label);
+}
+
+void *generate_instruction_code(op_code_t op_code, char *target, int n_args, ...)
+{
+	va_list args;
+	va_start(args, n_args);
+
+	printf("%s\t\t", operation2string(op_code));
+
+	switch(op_code)
+	{
+		// r1, r2 => r3
+		case ADD:
+		case SUB:
+		case MULT:
+		case DIV:
+		case LSHIFT:
+		case RSHIFT:
+		case AND:
+		case OR:
+		case XOR:
+		case LOADAO:
+		case CLOADAO:
+		case CMPLT:
+		case CMPLE:
+		case CMPEQ:
+		case CMPGE:
+		case CMPGT:
+		case CMPNE:
+			printf("%s, %s => %s\n", va_arg(args, char *), va_arg(args, char *), target);
+			break;
+		// r1 => r2, r3
+		case STOREAO:
+		case CSTOREAO:
+			printf("%s => %s, %s\n", va_arg(args, char *), target, va_arg(args, char *));
+			break;
+		// r1, c2 => r3
+		case ADDI:
+		case SUBI:
+		case RSUBI:
+		case MULTI:
+		case DIVI:
+		case RDIVI:
+		case LSHIFTI:
+		case RSHIFTI:
+		case ANDI:
+		case ORI:
+		case XORI:
+		case LOADAI:
+		case CLOADAI:
+			printf("%s, %s => %s\n", va_arg(args, char *), va_arg(args, char *), target);
+			break;
+		case STOREAI:
+			printf("%s => %s, %d\n", va_arg(args, char *), target, va_arg(args, int));
+			break;
+		case CSTOREAI:
+			printf("%s => %s, %d\n", va_arg(args, char *), target, va_arg(args, int));
+			break;
+		// r1 => r2
+		case LOAD:
+		case CLOAD:
+		case STORE:
+		case CSTORE:
+		case I2I:
+		case C2C:
+		case C2I:
+		case I2C:
+			printf("%s => %s\n", va_arg(args, char *), target);
+			break;
+		// => r1
+		case JUMP:
+			printf("=> %s\n", target);
+			break;
+		// c1 => r2
+		case LOADI:
+			printf("%s => %s\n", va_arg(args, char *), target);
+			break;
+		// r1 => l2, l3
+		case CBR:
+			printf("%s => %s, %s\n", target, va_arg(args, char *), va_arg(args, char *));
+			break;
+		// => l1
+		case JUMPI:
+			printf("=> %s\n", va_arg(args, char *));
+			break;
+	}
+
+	va_end(args);
+}
+
+char *operation2string(op_code_t op_code)
+{
+	switch (op_code)
+	{
+		case ADD:		return "add";
+		case SUB:		return "sub";
+		case MULT:		return "mult";
+		case DIV:		return "mult";
+		case LSHIFT:	return "lshift";
+		case RSHIFT:	return "rshift";
+		case LOADAO:	return "loadAO";
+		case CLOADAO:	return "cloadAO";
+		case STOREAO:	return "storeAO";
+		case CSTOREAO:	return "cstoreAO";
+		case CMPLT:		return "cmp_LT";
+		case CMPLE:		return "cmp_LE";
+		case CMPEQ:		return "cmp_EQ";
+		case CMPGE:		return "cmp_GE";
+		case CMPGT:		return "cmp_GT";
+		case CMPNE:		return "cmp_NE";
+		case ADDI:		return "addI";
+		case SUBI:		return "subI";
+		case RSUBI:		return "rsubI";
+		case MULTI:		return "multI";
+		case DIVI:		return "divI";
+		case RDIVI:		return "rdivI";
+		case LSHIFTI:	return "lshiftI";
+		case RSHIFTI:	return "rshiftI";
+		case LOADAI:	return "loadI";
+		case CLOADAI:	return "cloadAI";
+		case STOREAI:	return "storeAI";
+		case CSTOREAI:	return "cstoreAI";
+		case LOAD:		return "load";
+		case CLOAD:		return "cload";
+		case STORE:		return "store";
+		case CSTORE:	return "cstore";
+		case I2I:		return "i22";
+		case C2C:		return "c2c";
+		case C2I:		return "c2i";
+		case I2C:		return "i2c";
+		case JUMP:		return "jump";
+		case LOADI:		return "loadI";
+		case CBR:		return "cbr";
+		case JUMPI:		return "jumpI";
+		case AND:		return "and";
+		case ANDI:		return "andI";
+		case OR:		return "or";
+		case XOR:		return "xor";
+		case ORI:		return "orI";
+		case XORI:		return "orI";
+		case NOP:
+		default:		return "nop";
+	}
 }
